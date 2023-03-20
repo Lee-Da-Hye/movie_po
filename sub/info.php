8d6e8ec98f15f46da70084608d08b37613d72730
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-20 21:14:26 +0900

    php

--- sub/info.php
+++ sub/info.php
@@ -6,4 +6,12 @@
     </section>
 
     <button class="goPrevPage"><i class='bx bx-left-arrow-circle'></i></button>
+    <script>
+        async function loadMovieDetail(){
+        const movie = await JSON.parse(localStorage.getItem('info'));
+        console.log(movie);
+        document.querySelector('.detail-container').innerHTML = createDom(movie);
+    }
+    loadMovieDetail();
+    </script>
     <script src="/src/info.js?<?=time()?>"></script>
