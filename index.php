d9c09c612ae11755e723d49bc6052b2ba8c96037
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-21 00:06:25 +0900

    php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
 />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<script type="text/javascript" src="/src/common.js"></script>
+<script type="text/javascript" src="/src/common.js?<?=time()?>"></script>
 </head>
 <body>
 <!-- 상단 영역 -->
