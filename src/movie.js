66266b3092c89382a5063f6b11e0ba4aa3f4415f
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-21 00:42:24 +0900

    php

--- src/movie.js
+++ src/movie.js
@@ -143,3 +143,21 @@ labels.forEach((label, index)=>{
 })
 labels[0].click();
 
+/*createDom*/
+function createDom( movie, newDom){
+    let str = `
+        <div class="movie-box">
+            <div class="movie-img"><img src="${movie.medium_cover_image}" alt="${movie.title}" title = "${movie.summary}"></div>
+            <div class="movie-details">
+                <h3 class="title">${movie.title}</h3>
+            </div>       
+            <div class="flex">
+                <button id="${movie.id}" class='detail-btn'>영화정보</button>
+                <span class="reservation"><a href="#">예매하기</a></span>
+            </div>
+        </div>
+
+        `
+    return str;
+        
+}
