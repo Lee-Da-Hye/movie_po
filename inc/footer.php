b7aa04274e703decc60767f73571767716b28aa5
Lee-Da-Hye <dlekgp6521@naver.com>
2024-02-03 02:53:39 +0900

    modify

--- inc/footer.php
+++ inc/footer.php
@@ -2,12 +2,12 @@
         <div class="footer-bottom marginAuto">
             <h1>movie</h1>
             <address>
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
-                <span>TEL : 02-0000-0000/</span>
-                <span>FAX : 02-0000-0000<br></span>
+                <span>서울시 노원구 동일로 158길 33 </span><br>
+                <span>대표자명 : 이다혜</span> <span>TEL : 02-1234-5678/</span>
+                <span>FAX : 02-1234-5678<br></span>
             </address>
             <div class="footer-copyright">
                 <p class="right">개인정보처리방침</p>
-                <p>Copyright © Lorem ipsum dolor All rights reserved</p>
+                <p>Copyright © movie All rights reserved</p>
             </div>
         </div>
